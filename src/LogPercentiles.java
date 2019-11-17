1411a659d763ac5771d7660b27e36d5ae667147c
chenyiwen <chenyiwenglish@163.com>
2019-11-17 16:18:40 +0800

    riot homework - System Design

---
+++ src/LogPercentiles.java
@@ -0,0 +1,12 @@
+public class LogPercentiles {
+
+    public static void main(String args[]) {
+        // 在main.sh中实现 implement in main.sh
+        // 将大文件切割为多个小文件 try to split big file into smaller file
+        // 循环读小文件内容入内存，排序并写入新的小文件 loop to read file content into memory, write into new file after sort
+        // 多个小文件进行多路归并 merge sorted files
+        // 读文件行数，计算百分比落在哪一行 calculate line
+        // 取那一行读请求时间 get response time
+    }
+
+}
---
+++ src/SystemDesignForDada.java
@@ -0,0 +1,6 @@
+public class SystemDesignForDada {
+
+    public static void main(String args[]) {
+
+    }
+}
